f25cac6a96c3d196f608920797278f573363e67b
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-16 06:01:56 +0100

    set up project

---
+++ WidgetKitSandbox/AppMain.swift
@@ -0,0 +1,17 @@
+//
+//  AppMain.swift
+//  WidgetKitSandbox
+//
+//  Created by Zaid Neurothrone on 2022-12-16.
+//
+
+import SwiftUI
+
+@main
+struct AppMain: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
