0d3af878407381e3d77f936e913928b207a50443
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-16 06:01:09 +0100

    Initial Commit

---
+++ WidgetKitSandbox/WidgetKitSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetKitSandboxApp.swift
+//  WidgetKitSandbox
+//
+//  Created by Zaid Neurothrone on 2022-12-16.
+//
+
+import SwiftUI
+
+@main
+struct WidgetKitSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
